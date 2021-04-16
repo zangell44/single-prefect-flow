770e4847984481b85e1db14d8b652539d6e6defc
Zach Angell <42625717+zangell44@users.noreply.github.com>
2021-04-16 08:06:02 -0400

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,10 +1,14 @@
-import prefect
 from prefect import task, Flow
+from prefect.storage import GitRepo
+
+storage = GitRepo(file_path='flow.py', git_url='https://github.com/zangell44/single-prefect-flow.git')
 
 @task
 def say_hello(name):
-	logger = prefect.context.get("logger")
-	logger.info('Hi' + name)
-	
+        logger = prefect.context.get("logger")
+        logger.info('Hi' + name)
+
 with Flow('my-hello-flow') as flow:
-	say_hello('Zach')
+        say_hello('Zach')
+
+flow.storage = storage
