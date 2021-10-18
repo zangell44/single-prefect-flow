1d9eb75fe2c8077a6fb6a05899bfb04005831a53
Zach Angell <42625717+zangell44@users.noreply.github.com>
2021-10-18 10:00:22 -0400

    Update flow.py

--- flow.py
+++ flow.py
@@ -2,7 +2,10 @@ import prefect
 from prefect import task, Flow, Parameter
 from prefect.storage import Git
 
+#storage = Git(flow_path='flow.py', repo='zangell44/single-prefect-flow', git_token_secret_name="SUPAH_SECRET_GH_TOKEN") # commit='4854091758c9ca28766a60abeae88e66be0e9b63')
 storage = Git(flow_path='flow.py', repo='zangell44/single-prefect-flow')
+
+#name = 'foo'
 
 @task
 def say_hello(name):
@@ -10,9 +13,9 @@ def say_hello(name):
 	logger.info('Hi' + name)
 	
 with Flow('my-hello-flow') as flow:
-	name = Parameter(name, default='Zach')
+	name = Parameter(name="name", default='Zach')
 	say_hello(name)
 
 flow.storage = storage
 flow.register('test')
-#flow.run(
+#flow.run()
